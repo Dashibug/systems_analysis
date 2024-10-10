236175acb12fb4abf8ab0f0bede85fd359181c0c
Dashibug <megres1337@mail.ru>
2024-10-10 13:23:21 +0300

    task3
    task3

---
+++ task3/task3.py
@@ -0,0 +1,33 @@
+import csv
+import math
+
+
+def calculate_entropy(csv_str: str) -> float:
+
+    lines = csv_str.strip().split('\n')
+    reader = csv.reader(lines)
+
+    data = [list(map(int, row)) for row in reader]
+    row_count = len(data)
+
+    total_entropy = 0
+
+    for row in data:
+        for value in row:
+            if value > 0:
+                probability = value / (row_count - 1)
+                total_entropy += -probability * math.log2(probability)
+
+    return round(total_entropy, 1)
+
+
+if __name__ == "__main__":
+    csv_input = """1,0,4,0,0
+                   2,1,2,0,0
+                   2,1,0,1,1
+                   0,1,0,1,1
+                   0,1,0,2,1
+                   0,1,0,2,1"""
+
+    result = calculate_entropy(csv_input)
+    print(f"Энтропия: {result}")
