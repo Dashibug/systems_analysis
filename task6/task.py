817a1e354b6eb0092d7803745050e38b489705ea
Dashibug <megres1337@mail.ru>
2025-01-03 19:35:28 +0300

    fix_6
    fix_6

--- task6/task.py
+++ task6/task.py
@@ -1,123 +1,115 @@
+import argparse
 import json
+import numpy as np
+import os
+import skfuzzy as fuzz
+from skfuzzy import control as ctrl
 
 
-def interpolate(x, points):
-    for i in range(len(points) - 1):
-        x1, y1 = points[i]
-        x2, y2 = points[i + 1]
-        if x1 <= x <= x2:
-            return y1 + (y2 - y1) * (x - x1) / (x2 - x1)
-    return 0
-
-
-def membership_function(value, fuzzy_set):
-    return interpolate(value, fuzzy_set)
-
-
-def get_membership(value, fuzzy_sets):
-    return {term["id"]: membership_function(value, term["points"]) for term in fuzzy_sets}
-
-
-def apply_rules(temp_degree, heating_degrees, mapping):
-    result = 0
-    total_weight = 0
-
-    for temp_term, heat_term in mapping:
-        temp_value = temp_degree.get(temp_term, 0)
-        heat_value = heating_degrees.get(heat_term, 0)
-
-        weight = min(temp_value, heat_value)
-        result += weight * heat_value
-        total_weight += weight
-
-    return result / total_weight if total_weight > 0 else 0
-
-
-def task(temp_json, heat_json, rules_json, current_temperature):
-    temp_data = json.loads(temp_json)["температура"]
-    heat_data = json.loads(heat_json)["температура"]
+def task(temp_mf_json, heat_mf_json, rules_json, current_temp):
+    temp_mfs = json.loads(temp_mf_json)
+    heat_mfs = json.loads(heat_mf_json)
     rules = json.loads(rules_json)
 
-    temp_degree = get_membership(current_temperature, temp_data)
-    heating_degree = get_membership(current_temperature, heat_data)
+    min_temp = float('inf')
+    max_temp = float('-inf')
 
-    result = apply_rules(temp_degree, heating_degree, rules)
+    min_heat = float('inf')
+    max_heat = float('-inf')
 
-    return result
+    for state in temp_mfs["температура"]:
+        points = state["points"]
+        for point in points:
+            temperature = point[0]
+            min_temp = min(min_temp, temperature)
+            max_temp = max(max_temp, temperature)
+
+    for state in heat_mfs["уровень нагрева"]:
+        points = state["points"]
+        for point in points:
+            temperature = point[0]
+            min_heat = min(min_temp, temperature)
+            max_heat = max(max_temp, temperature)
+
+    temperature = ctrl.Antecedent(np.arange(min_temp, max_temp, 1), 'temperature')
+    heating = ctrl.Consequent(np.arange(min_heat, max_heat, 0.1), 'heating')
+
+    for mf in temp_mfs['температура']:
+        points = np.array(mf['points'])
+        temperature[mf['id']] = fuzz.trapmf(temperature.universe, [points[0][0], points[1][0], points[2][0], points[3][0]])
+
+    for mf in heat_mfs['уровень нагрева']:
+        points = np.array(mf['points'])
+        heating[mf['id']] = fuzz.trapmf(heating.universe, [points[0][0], points[1][0], points[2][0], points[3][0]])
+
+    activated_rules = []
+    for rule in rules:
+        if rule[0] in temperature.terms and rule[1] in heating.terms:
+            temp_level = fuzz.interp_membership(temperature.universe, temperature[rule[0]].mf, current_temp)
+            # print(f"Сработало правило: если {rule[0]} то {rule[1]} с уровнем активации {temp_level:.2f}")
+            if temp_level > 0:
+                activated_rules.append((temp_level, rule[1]))
+
+    output_mf = np.zeros_like(heating.universe)
+    for activation_level, heat_term in activated_rules:
+        heat_mf = heating[heat_term].mf
+        output_mf = np.maximum(output_mf, np.minimum(activation_level, heat_mf))
+    if np.any(output_mf):
+        defuzzified_output = fuzz.defuzz(heating.universe, output_mf, 'centroid')
+        return defuzzified_output
+    else:
+        raise ValueError("выходная область пуста")
+
+
+def load_json(source, default):
+    try:
+        if os.path.isfile(source):
+            with open(source, 'r') as file:
+                return json.load(file)
+        return json.loads(source)
+    except Exception as e:
+        return default
 
 
 if __name__ == "__main__":
-    temp_json = '''{
-      "температура": [
-          {
-          "id": "холодно",
-          "points": [
-              [0,1],
-              [18,1],
-              [22,0],
-              [50,0]
-          ]
-          },
-          {
-          "id": "комфортно",
-          "points": [
-              [18,0],
-              [22,1],
-              [24,1],
-              [26,0]
-          ]
-          },
-          {
-          "id": "жарко",
-          "points": [
-              [0,0],
-              [24,0],
-              [26,1],
-              [50,1]
-          ]
-          }
-      ]
-    }'''
+    default_temp_mf = {
+        "температура": [
+            {"id": "холодно", "points": [[0, 0], [5, 1], [10, 1], [12, 0]]},
+            {"id": "комфортно", "points": [[18, 0], [22, 1], [24, 1], [26, 0]]},
+            {"id": "жарко", "points": [[0, 0], [24, 0], [26, 1], [40, 1], [50, 0]]}
+        ]
+    }
 
-    heat_json = '''{
-      "температура": [
-          {
-            "id": "слабый",
-            "points": [
-                [0,0],
-                [0,1],
-                [5,1],
-                [8,0]
-            ]
-          },
-          {
-            "id": "умеренный",
-            "points": [
-                [5,0],
-                [8,1],
-                [13,1],
-                [16,0]
-            ]
-          },
-          {
-            "id": "интенсивный",
-            "points": [
-                [13,0],
-                [18,1],
-                [23,1],
-                [26,0]
-            ]
-          }
-      ]
-    }'''
+    default_heat_mf = {
+        "уровень нагрева": [
+            {"id": "слабый", "points": [[0, 0], [0, 1], [5, 1], [8, 0]]},
+            {"id": "умеренный", "points": [[5, 0], [8, 1], [13, 1], [16, 0]]},
+            {"id": "интенсивный", "points": [[13, 0], [18, 1], [23, 1], [26, 0]]}
+        ]
+    }
 
-    rules_json = '''[
-      ["холодно", "интенсивный"],
-      ["комфортно", "умеренный"],
-      ["жарко", "слабый"]
-    ]'''
+    default_rules = [
+        ['холодно', 'интенсивный'],
+        ['комфортно', 'умеренный'],
+        ['жарко', 'слабый']
+    ]
 
-    current_temperature = 20
+    default_current_temp = 15
 
-    result = task(temp_json, heat_json, rules_json, current_temperature)
-    print(f"Значения оптимального управления: {result}")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--temp_file', type=str)
+    parser.add_argument('--heat_file', type=str)
+    parser.add_argument('--rules_file', type=str)
+    parser.add_argument('--current_temp', type=int, default=default_current_temp)
+    args = parser.parse_args()
+
+    temp_mf_json = load_json(args.temp_file, json.dumps(default_temp_mf))
+    heat_mf_json = load_json(args.heat_file, json.dumps(default_heat_mf))
+    rules_json = load_json(args.rules_file, json.dumps(default_rules))
+    current_temp = args.current_temp
+
+    try:
+        optimal_heating = task(temp_mf_json, heat_mf_json, rules_json, current_temp)
+        print(f"{optimal_heating:.2f}")
+    except ValueError as e:
+        print(f"Ошибка: {e}")
